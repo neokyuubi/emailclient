93c9a67157ad8fa8229a64d576a9a3c0625d8baa
Neokyuubi <neokyuubi@gmail.com>
2022-09-26 23:24:23 +0100

    guard

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Route, UrlSegment, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanLoad, Route, Router, UrlSegment, UrlTree } from '@angular/router';
+import { Observable, skipWhile, take, tap } from 'rxjs';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanLoad {
 
-
+  constructor(private authService:AuthService, private router:Router){}
 
   canLoad(route: Route, segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree
   {
-    console.log(route);
-    return true;
+    console.log("guard");
+    // return true
+    return this.authService.signedIn$.pipe(
+      skipWhile(value=> value == null),
+      take(1),
+      tap((authenticated)=>{
+        if(!authenticated)
+        {
+          this.router.navigateByUrl("/");
+        }
+      })
+      )
   }
 }
